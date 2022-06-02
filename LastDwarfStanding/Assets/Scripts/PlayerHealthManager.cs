9d905628280e051bff289bb76580d7e0d6f71f82
AIE\s213321 <s213321@students.aie.edu.au>
2022-06-02 13:10:50 +0930

    02/06/22 Added PlayerHeathManager Script (Linked to UI elements) Added Base for Currency to be implimented (Including animated 2D Sprite coin)

---
+++ LastDwarfStanding/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float maxHealth;
+    public float currentHealth;
+
+    private float _timeSinceDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.maxValue = currentHealth;
+        _UpdateHealthBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeSinceDamage += Time.deltaTime;
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        currentHealth -= damageToTake;
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+    public void RecieveHealth(float healthToRecieve)
+    {
+
+        currentHealth += healthToRecieve;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        _timeSinceDamage = 0;
+        _UpdateHealthBar();
+    }
+    private void _UpdateHealthBar()
+    {
+        healthSlider.value = currentHealth;
+    }
+}
