d701c0877e1242a67edf63921eaad34dbf98fb33
EUANS-PC\Euan Jones <s213321@students.aie.edu.au>
2022-07-05 12:12:16 +0930

    05/07/22 more number adjustments to enemymelees

--- LastDwarfStanding/Assets/Scripts/Enemy/EnemyNavigationManager.cs
+++ LastDwarfStanding/Assets/Scripts/Enemy/EnemyNavigationManager.cs
@@ -199,7 +199,7 @@ public class EnemyNavigationManager : MonoBehaviour
         }
         else
         {
-            print(name + "not getting any hits");
+            //print(name + "not getting any hits");
         }
 
         return true;
